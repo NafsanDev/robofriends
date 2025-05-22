aecdb3396a442bed0542e1ab2a970f265094739f
Nur Mohammad <nur.m.sohel@gmail.com>
2025-05-23 05:50:35 +0600

    Card component form an Array

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import Hello from './Hello';
-import Card from './Card.jsx';
+import CardList from './CardList.jsx';
 import Footer from './Footer.jsx';
 import {robots} from './robots'
 import 'tachyons';
 
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Hello/>
-    <Card id = {robots[0].id} name = {robots[0].name} email= {robots[0].email}/>
+    <CardList robots={robots}/>
     <Footer/>
   </StrictMode>,
 )
