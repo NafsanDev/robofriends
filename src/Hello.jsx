f5c5c1e58a5f8cd2302c3cbe82a05f0d8735aaf3
Nur Mohammad <nur.m.sohel@gmail.com>
2025-05-22 07:28:54 +0600

    Hello World

---
+++ src/Hello.jsx
@@ -0,0 +1,15 @@
+import { Component } from "react";
+
+
+class Hello extends Component{
+    render(){
+        return(
+        <div>
+            <h1>Hello Noor!</h1>
+            <p>Welcome to Queens World of Developer.</p>
+        </div>
+        )
+    }
+}
+
+export default Hello;
