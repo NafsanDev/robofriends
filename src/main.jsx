c37852dfd0d4fb87e0a2fc2580456a9d4a183235
Nur Mohammad <nur.m.sohel@gmail.com>
2025-05-23 06:12:29 +0600

    Creating App.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import Hello from './Hello';
-import CardList from './CardList.jsx';
-import Footer from './Footer.jsx';
-import {robots} from './robots'
+import App from './App';
 import 'tachyons';
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Hello/>
-    <CardList robots={robots}/>
-    <Footer/>
+    <App/>
   </StrictMode>,
 )
